5b7b8a599eaa211f77883020f427c6b49931a93f
rorschach777 <msweitzer1987@gmail.com>
2020-07-06 06:55:27 -0400

    second commit

---
+++ src/Components/Container16x9/Container16x9.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Container16x9 = (props) => {
+    return (
+        <div class="Container16x9">
+            {props.children}
+        </div>
+    );
+};
+
+export default Container16x9;
