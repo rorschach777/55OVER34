16d94f2d07a931ccd3f905fd2621e9ffa0924597
rorschach777 <msweitzer1987@gmail.com>
2020-07-06 10:43:07 -0400

    aws

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.scss';
-import intro from "./Content/Videos/55OVER34-1280x720.mp4";
+
 import Container16x9 from './Components/Container16x9/Container16x9';
 import Video from './Components/Video/Video';
 import Typography from  './Components/Typography/Typography';
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Typography>
         <Container16x9>
-          <Video video={intro}/>
+          <Video video="https://www.youtube.com/watch?v=erQ_9yEz0ls"/>
         </Container16x9>
         <h6>WWW.55OVER34.COM | COMING SOON</h6>
       </Typography>
