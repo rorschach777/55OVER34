86a31d3ea7a4cefee628b00faa386fd9a6334629
rorschach777 <msweitzer1987@gmail.com>
2020-07-06 16:26:40 -0400

    video updated

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Video.scss';
 const Video = (props) => {
     return (
-        <video autoPlay loop muted playsinline className="Video">
+        <video autoplay muted playsinline loop className="Video" >
             <source src={props.video} type="video/mp4"></source>
             Sorry, your browser doesn't support embedded videos.
         </video>
