19f477df1137b677d5805f8a410b216d852168f1
rorschach777 <msweitzer1987@gmail.com>
2020-08-03 08:54:53 -0400

    changed logo / removed video

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import './App.scss';
-import intro from "./Content/Videos/55OVER34-1280x720.mp4";
+
 import Container16x9 from './Components/Container16x9/Container16x9';
 import Video from './Components/Video/Video';
+import Logo from './Content/images/logo-2.png';
 
 function App() {
   return (
     <div className="App">
-        <Container16x9>
-          <Video video={intro}/>
-        </Container16x9>
+
+          <img className="Logo" src={Logo}/>
+
         <p>WWW.55OVER34.COM | COMING SOON</p>
     </div>
   );
